a97b8da37a1fa92f2ef963820a25c04fde5ab630
deo86 <131874033+deo86@users.noreply.github.com>
2023-04-30 15:34:38 +0300

    Number of visits counter

--- wp-content/plugins/softuni-homes/functions.php
+++ wp-content/plugins/softuni-homes/functions.php
@@ -1,6 +1,11 @@
 <?php
 
-
+/**
+ * Display time ago string
+ *
+ * @param [type] $type
+ * @return string
+ */
 function softuni_time_ago( $type = 'post' ) {
     $d = 'comment' == $type ? 'get_comment_time' : 'get_post_time';
 
@@ -58,3 +63,120 @@ function softuni_update_home_views_count( $home_id ) {
     update_post_meta( $home_id, 'views_count', $view_count );
 
 }
+
+/**
+ * Homes Enqueue
+ */
+function softuni_enqueue_scripts() {
+	wp_enqueue_script( 'softuni-script', plugins_url( 'assets/scripts/scripts.js', __FILE__ ), array( 'jquery' ), 1.1 );
+	wp_localize_script( 'softuni-script', 'my_ajax_object', array( 'ajax_url' => admin_url( 'admin-ajax.php' ) ) );
+
+}
+add_action( 'wp_enqueue_scripts', 'softuni_enqueue_scripts' );
+
+/**
+ * Functions takes care of the like of the home
+ *
+ * @return void
+ */
+function softuni_home_like() {
+	$home_id = esc_attr( $_POST['home_id'] );
+
+	$like_number = get_post_meta( $home_id, 'likes', true );
+
+    if ( empty( $like_number ) ) {
+        update_post_meta( $home_id, 'likes', 1 );
+    } else {
+        $like_number = $like_number + 1;
+        update_post_meta( $home_id, 'likes', $like_number );
+    }
+
+    wp_die();
+}
+add_action( 'wp_ajax_nopriv_softuni_home_like', 'softuni_home_like' );
+add_action( 'wp_ajax_softuni_home_like', 'softuni_home_like' );
+
+/**
+ * Displays the current user name if the user is logged in
+ *
+ * @return void
+ */
+function softuni_display_username() {
+    $output = '';
+
+    if ( is_user_logged_in() == true ) {
+        $current_user = wp_get_current_user();
+        $user_display_name = $current_user->data->display_name;
+        $output = 'Hello, ' . $user_display_name . ', enjoy the article!';
+    }
+
+    return $output;
+}
+add_shortcode( 'display_username', 'softuni_display_username' );
+
+/**
+ * It gets the content and counts the number of words
+ *
+ * @return void
+ */
+function softunit_display_post_word_count( $atts ) {
+    $output = '';
+    $word_count = 0;
+    $post_id = '';
+
+    $attributes = shortcode_atts( array(
+		'post_id' => '',
+	), $atts );
+
+    if ( ! empty( $attributes['post_id'] ) ) {
+        $post_id = $attributes['post_id'];
+
+        $post = get_post( $attributes['post_id'] );
+        if ( ! empty( $post ) ) {
+            // @TODO: we have to strip the markup and Gutenberg items so we have a better result.
+            $post_content = $post->post_content;
+            $word_count = str_word_count( $post_content );
+        }
+
+    } else {
+        $output = 'You must add a post_id as an attribute.';
+    }
+
+    if ( ! empty( $word_count ) ) {
+        $output = 'The number of words for the Post ID ' . $post_id . ' is ' . $word_count;
+    }
+
+    return $output;
+}
+add_shortcode( 'display_post_word_count', 'softunit_display_post_word_count' );
+
+function softuni_update_home_visit_count( $post_id = 0 ) {
+    if ( empty( $post_id ) ) {
+        return;
+    }
+
+    $visit_count = get_post_meta( $post_id, 'visits_count', true );
+
+    if ( ! empty( $visit_count ) ) {
+        $visit_count = $visit_count + 1;
+
+        update_post_meta( $post_id, 'visits_count', $visit_count );
+    } else {
+        update_post_meta( $post_id, 'visits_count', 1 );
+    }
+}
+
+// Old code from the initial sessions
+ /**
+  * Undocumented function
+  *
+  * @param [type] $title
+  * @return void
+  */
+  function change_title_text( $title ) {
+
+    var_dump( $title );
+
+    return $title . ' 1st function' ;
+}
+// add_filter( 'the_title', 'change_title_text', 10 );
--- wp-content/themes/softuni-homes/single-home.php
+++ wp-content/themes/softuni-homes/single-home.php
@@ -6,50 +6,47 @@
 
 		<?php the_post(); ?>
 
-		<div class="job-single">
-			<main class="job-main">
-				<div class="job-card">
-					<div class="job-primary">
-						<header class="job-header">
-							<h1 class="job-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-							<div class="job-meta">
-								<a class="meta-company" href="#"><?php echo softuni_display_single_term( get_the_ID(), 'company' ); ?></a>
+		<div class="home-single">
+			<main class="home-main">
+				<div class="property-card">
+					<div class="home-primary">
+						<header class="home-header">
+							<h1 class="home-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+							<div class="home-meta">
+								<a class="property-meta" href="#"><?php echo softuni_display_single_term( get_the_ID(), 'neighborhood' ); ?></a>
+								<a class="property-meta" href="#"><?php echo softuni_display_single_term( get_the_ID(), 'location' ); ?></a>
+								<div class="meta-date">Property visits: <?php echo get_post_meta( get_the_ID(), 'visits_count', true ); ?></div>
 								<span class="meta-date">Posted on <?php echo get_the_date(); ?></span>
-								<span class="meta-date">Jobs visits: <?php echo get_post_meta( get_the_ID(), 'visits_count', true ); ?></span>
 							</div>
-							<div class="job-details">
-								<span class="job-location"><?php echo softuni_display_single_term( get_the_ID(), 'location' ); ?></span>
-								<span class="job-type"><?php the_author(); ?></span>
+							<div class="property-details">
+								<span class="home-type">Author: <?php the_author(); ?></span>
 							</div>
 						</header>
 
-						<div class="job-body">
+						<div class="home-body">
 							<?php the_content(); ?>
 						</div>
 					</div>
 				</div>
 			</main>
-			<aside class="job-secondary">
-				<div class="job-logo">
-					<div class="job-logo-box">
+			<aside class="home-secondary">
+				<div class="home-logo">
+					<div class="home-logo-box">
 						<?php if ( has_post_thumbnail() ) :  ?>
-							<?php the_post_thumbnail( 'job-thumbnail' ); ?>
+							<?php the_post_thumbnail( 'home-thumbnail' ); ?>
 						<?php else : ?>
 							<img src="https://i.imgur.com/ZbILm3F.png" alt="default image">
 						<?php endif; ?>
 					</div>
 				</div>
 				<a id="<?php echo get_the_ID(); ?>" class="like-button" href="#">Like (<?php echo get_post_meta( get_the_ID(), 'likes', true ) ?>)</a>
-				<a href="#" class="button button-wide">Apply now</a>
-				<a href="#">apple.com</a>
+				<a href="#" class="button button-wide">Buy now</a>
 			</aside>
 		</div>
 
-		<h2 class="section-heading">Other jobs from the company:</h2>
+		<h2 class="section-heading">Other properties from the company:</h2>
 
-		<?php echo softuni_display_other_jobs_per_company( get_the_ID() ); ?>
-
-		<?php softuni_update_job_visit_count( get_the_ID() ) ?>
+		<?php softuni_update_home_visit_count( get_the_ID() ) ?>
 
 	<?php endwhile; ?>
 
